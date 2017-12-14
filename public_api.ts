89e67bbe2a397fd2d7c1efa800990f84e1258a86
Nathan Kern <natjkern@gmail.com>
2017-12-13 20:25:27 -0600

    add provider to ngais module

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,5 @@
 export * from './src/app/modules/tft-header/tft-header.module';
-export * from './src/app/modules/instantsearch/instantsearch.module';
-export * from './src/app/modules/results/results.module';
+export * from './src/app/modules/index';
+
+// export * from './src/app/modules/instantsearch/instantsearch.module';
+// export * from './src/app/modules/results/results.module';
