f4f8e720212e74f61d597c2146fae2cf69b92cf2
Nathan Kern <natjkern@gmail.com>
2018-01-27 21:36:39 -0600

    testing test of update_type

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,8 @@ then
   exit 1
 fi
 
+# TODO: Request git status and ask user to commit changes
+
 # # read actual dist/package.json version
 # actual_version=$(grep version package.json | cut -c 15- | rev | cut -c 3- | rev)
 
@@ -23,16 +25,19 @@ update_options=(patch minor major)
 echo ${update_options[*]}
 read update_type
 
-# if [ "$update_type" != "patch"] #|| ["$update_type" != "minor"] || ["$update_type" != "major" ]
-# then
-#   echo "Not a valid semantic update, try 'patch', 'minor', or 'major'"
-#   exit 1
-# fi
+if [ "$update_type" != "patch"] #|| ["$update_type" != "minor"] || ["$update_type" != "major" ]
+then
+  echo "Not a valid semantic update, try 'patch', 'minor', or 'major'"
+  exit 1
+fi
 npm version "${update_type}"
 
 npm run packagr
 
+# TODO: Maybe pass algolia image with library 'assets/images/, maybe serve from cdn
 npm publish dist
+
+# TODO: 
 # # replace package.json with next version
 # # sed -i.bak "s/${actual_version}/${next_version}/g" src/version.ts
 # # sed -i.bak "s/${actual_version}/${next_version}/g" dist/package.json
